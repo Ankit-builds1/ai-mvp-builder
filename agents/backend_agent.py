ebc56a42a5043ca05c2823b88643f31c2f652cfd
ANKIT DASH <005ankitdash@gmail.com>
2026-05-28 14:40:12 +0530

    Create backend_agent.py

---
+++ agents/backend_agent.py
@@ -0,0 +1,36 @@
+from google import genai
+
+# Configure Gemini client
+client = genai.Client(api_key="AIzaSyDspZ9klOOAV-2XbilSItRpxphfS0OvU_o")
+
+
+def backend_agent(project_idea):
+
+    prompt = f"""
+    Generate a professional FastAPI backend structure for:
+    {project_idea}
+
+    Include:
+    - API routes
+    - CRUD operations
+    - proper FastAPI code
+    - clean architecture
+    """
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=prompt
+    )
+
+    return response.text
+
+
+if __name__ == "__main__":
+
+    idea = input("Enter your app idea: ")
+
+    result = backend_agent(idea)
+
+    print("\nGenerated Backend:\n")
+
+    print(result)
